1283886c7716648555140c11ee10d508518f9a85
DESKTOP-7DOV5GB\charg <charg2dang@gmail.com>
2020-12-20 21:56:57 +0900

    End Of Chapter 4

---
+++ ArenaBattle426/Source/ArenaBattle426/ABPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ArenaBattle426.h"
+#include "GameFramework/PlayerController.h"
+#include "ABPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLE426_API AABPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	virtual void PostInitializeComponents() override final;
+	virtual void OnPossess(APawn* aPawn) override final;
+};
